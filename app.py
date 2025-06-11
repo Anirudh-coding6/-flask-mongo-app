5119deb6689b77aab382c284fc4e37fd1344b8f8
Anirudh Vernekar <anirudhvernekar31@gmail.com>
2025-06-11 23:59:09 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# # Local MongoDB (installed version)
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+# client = MongoClient(MONGO_URI)
+# db = client["mydatabase"]
+# collection = db["users"]
+
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client["mydatabase"]
+collection = db["users"]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        collection.insert_one({"name": name, "email": email})
+        return redirect("/")
+    users = collection.find()
+    return render_template("index.html", users=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
